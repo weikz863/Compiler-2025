f467752e2fa8df3647f10aad382dbf32b6272082
weikz <weikz@sjtu.edu.cn>
2025-08-23 22:07:27 +0800

    fix: more tests

--- tests/lexer_test.cpp
+++ tests/lexer_test.cpp
@@ -32,3 +32,108 @@ TEST(LexerTest, ReadCharacters) {
   EXPECT_EQ(c7, 'b');
   EXPECT_EQ(len7, 1);
 }
+
+TEST(LexerTest, KeywordLex) {
+  auto tokens = lex("let");
+  ASSERT_EQ(tokens.size(), 1);
+  auto token = tokens[0];
+  EXPECT_EQ(token.type, Token::Type::Keyword);
+  EXPECT_EQ(token.value, "let");
+}
+
+TEST(LexerTest, IdentifierLex) {
+  auto tokens = lex("x");
+  ASSERT_EQ(tokens.size(), 1);
+  auto token = tokens[0];
+  EXPECT_EQ(token.type, Token::Type::Identifier);
+  EXPECT_EQ(token.value, "x");
+}
+
+TEST(LexerTest, IntegerLiteralLex) {
+  auto tokens = lex("42");
+  ASSERT_EQ(tokens.size(), 1);
+  auto token = tokens[0];
+  EXPECT_EQ(token.type, Token::Type::IntegerLiteral);
+  EXPECT_EQ(token.value, "42");
+}
+
+TEST(LexerTest, PunctuationLex) {
+  auto tokens = lex("=");
+  ASSERT_EQ(tokens.size(), 1);
+  auto token = tokens[0];
+  EXPECT_EQ(token.type, Token::Type::Punctuation);
+  EXPECT_EQ(token.value, "=");
+}
+
+TEST(LexerTest, StringLiteralLex) {
+  auto tokens = lex("\"hello, world!\"");
+  ASSERT_EQ(tokens.size(), 1);
+  auto token = tokens[0];
+  EXPECT_EQ(token.type, Token::Type::StringLiteral);
+  EXPECT_EQ(token.value, "\"hello, world!\"");
+}
+
+TEST(LexerTest, CharLiteralLex) {
+  auto tokens = lex("'a'");
+  ASSERT_EQ(tokens.size(), 1);
+  auto token = tokens[0];
+  EXPECT_EQ(token.type, Token::Type::CharLiteral);
+  EXPECT_EQ(token.value, "'a'");
+}
+
+TEST(LexerTest, EscapedCharLiteralLex) {
+  auto tokens = lex("'\\n'");
+  ASSERT_EQ(tokens.size(), 1);
+  auto token = tokens[0];
+  EXPECT_EQ(token.type, Token::Type::CharLiteral);
+  EXPECT_EQ(token.value, "'\\n'");
+}
+
+TEST(LexerTest, EscapedCharLiteralLex2) {
+  auto tokens = lex("'\"'");
+  ASSERT_EQ(tokens.size(), 1);
+  auto token = tokens[0];
+  EXPECT_EQ(token.type, Token::Type::CharLiteral);
+  EXPECT_EQ(token.value, "'\"'");
+}
+
+TEST(LexerTest, EscapedCharLiteralLex3) {
+  auto tokens = lex(R"("'\"\'\\")");
+  ASSERT_EQ(tokens.size(), 1);
+  auto token = tokens[0];
+  EXPECT_EQ(token.type, Token::Type::StringLiteral);
+  EXPECT_EQ(token.value, R"("'\"\'\\")");
+}
+
+TEST(LexerTest, CompLex) {
+  string input = R"(let x = 42;
+if x > 10 {
+  println!("x is greater than 10");
+} else {
+  println!("x is less than or equal to 10");
+}
+)";
+  auto tokens = lex(input);
+  std::vector<string> expected_values = {
+    "let", "x", "=", "42", ";",
+    "if", "x", ">", "10", "{",
+    "println", "!", "(", "\"x is greater than 10\"", ")", ";",
+    "}", "else", "{",
+    "println", "!", "(", "\"x is less than or equal to 10\"", ")", ";",
+    "}"
+  };
+  std::vector<Token::Type> expected_types = {
+    Token::Type::Keyword, Token::Type::Identifier, Token::Type::Punctuation, Token::Type::IntegerLiteral, Token::Type::Punctuation,
+    Token::Type::Keyword, Token::Type::Identifier, Token::Type::Punctuation, Token::Type::IntegerLiteral, Token::Type::Punctuation,
+    Token::Type::Identifier, Token::Type::Punctuation, Token::Type::Punctuation, Token::Type::StringLiteral, Token::Type::Punctuation, Token::Type::Punctuation,
+    Token::Type::Punctuation, Token::Type::Keyword, Token::Type::Punctuation,
+    Token::Type::Identifier, Token::Type::Punctuation, Token::Type::Punctuation, Token::Type::StringLiteral, Token::Type::Punctuation, Token::Type::Punctuation,
+    Token::Type::Punctuation
+  };
+  ASSERT_EQ(tokens.size(), expected_types.size());
+  ASSERT_EQ(tokens.size(), expected_values.size());
+  for (size_t i = 0; i < tokens.size(); ++i) {
+    EXPECT_EQ(tokens[i].value, expected_values[i]);
+    EXPECT_EQ(tokens[i].type, expected_types[i]);
+  }
+}
