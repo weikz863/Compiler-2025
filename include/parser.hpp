b23acb466dcd6407e72c153f3a33a7db3403710b
weikz <weikz@sjtu.edu.cn>
2025-10-13 20:53:45 +0800

    fix: all compilability problems

--- include/parser.hpp
+++ include/parser.hpp
@@ -5,6 +5,7 @@
 
 #include <memory>
 #include <vector>
+#include <set>
 #include "lexer.hpp"
 #include "parse_rules.hpp"
 
--- src/parser.cpp
+++ src/parser.cpp
@@ -1,11 +1,11 @@
 #include "parser.hpp"
 #include <map>
 
-ParsingState::operator == (const ParsingState &other) const {
+bool ParsingState::operator == (const ParsingState &other) const {
   return std::tie(nonterminal_type, production_index, position_in_production, start_token_index) ==
          std::tie(other.nonterminal_type, other.production_index, other.position_in_production, other.start_token_index);
 }
-ParsingState::operator < (const ParsingState &other) const {
+bool ParsingState::operator < (const ParsingState &other) const {
   return std::tie(nonterminal_type, production_index, position_in_production, start_token_index) <
          std::tie(other.nonterminal_type, other.production_index, other.position_in_production, other.start_token_index);
 }
